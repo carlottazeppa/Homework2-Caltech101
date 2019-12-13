0d6a6244a88e182ee89c8854f9be432972cf5c17
carlottazeppa <45455476+carlottazeppa@users.noreply.github.com>
2019-12-13 17:36:35 +0100

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -15,7 +15,7 @@ def pil_loader(path):
 
 
 class Caltech(VisionDataset):
-    def __init__(self, root, split=split, transform=None, target_transform=None):
+    def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
         self.split = split # This defines the split you are going to use
